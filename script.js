7ff6b8a2e6f039520f61feb493cc1c71eb933bd3
LukaNikcevic77 <nikcevic.luka77@gmail.com>
2023-03-03 21:04:32 +0100

    Finish complete logic and main design

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 const board = document.getElementById('board');
+const scoreSheetp1 = document.getElementById('scoreSheetNamingp1');
+const scoreSheetp2 = document.getElementById('scoreSheetNamingp2');
 const fields = document.getElementsByClassName('field');
 const roundFields = document.getElementsByClassName('rndC');
 const player1Scores = document.getElementsByClassName('p1Score');
 const player2Scores = document.getElementsByClassName('p2Score');
+const form = document.getElementById('forma');
+const background = document.querySelector('.setnames');
+const p1nameInput = form.elements['player1name'];
+const p2nameInput = form.elements['player2name'];
 const fieldsArray = [...fields];
+const roundsFieldsArray = [...roundFields];
+const player1ScoresArray = [...player1Scores];
+const player2ScoresArray = [...player2Scores];
+const namesSetButton = document.querySelector("button");
 console.log(board);
+console.log(roundFields);
+console.log(background);
+
+namesSetButton.addEventListener('click', (e) => {
+    if(p1nameInput.value.length > 2 && p2nameInput.value.length > 2 && p1nameInput.value.length < 7 && p2nameInput.value.length < 7) {
+        setNames(p1nameInput, p2nameInput);
+    }
+})
+
 let gameBoard = {
 
     rowsAndColls: 3,
@@ -74,25 +93,48 @@ let gameBoard = {
                     gameBoard.hits = 2;
                     
                 }
+             
+             }
+             else if(y == 1) {
+                console.log("y == 1 was logged");
+                if(gameBoard.field[x][0] == sign && gameBoard.field[x][2] == sign){
+                    gameBoard.hits = 2;
+                }
              }
             // Checks top and bottom
             if(x - 2 > - 1) {
                 if(gameBoard.field[1][y] == sign && gameBoard.field[0][y] == sign){
+                    
                     gameBoard.hits = 2;
                     
                 }
             }
             else if(x + 2 < 3) {
+                
                 if(gameBoard.field[1][y] == sign && gameBoard.field[2][y] == sign){
                     gameBoard.hits = 2;
                     
                 }
             }
+            else if(x == 1) {
+                console.log("y == 1 was logged");
+                if(gameBoard.field[0][y] == sign && gameBoard.field[2][y] == sign){
+                    gameBoard.hits = 2;
+                }
+             }
             if(gameBoard.hits == 2){
                 console.log("YEy we got diagonal win broski");
                 board.style.backgroundColor = "green";
                 disablePlaying();
+                
                 gameController.updateScore(sign);
+            }
+            else if(gameBoard.hits != 2 && gameController.fullfields == 8){
+                console.log("Its a draw bitch");
+                board.style.backgroundColor = "orange";
+                disablePlaying();
+                
+                gameController.updateScore("draw");
             }
             else {
                 console.log("We got dick broski");
@@ -101,9 +143,17 @@ let gameBoard = {
         }
     }
 
-          
-            
-    
+function setNames(p1nameInput, p2nameInput) {
+    player1.name =  p1nameInput.value;
+    player2.name = p2nameInput.value;
+    background.classList.add("hideIt");
+    scoreSheetp1.textContent = p1nameInput.value;
+    scoreSheetp2.textContent = p2nameInput.value;
+
+   
+}    
+
+
 
     const playerMaker = (playerName, sign) => {
         name = playerName,
@@ -115,7 +165,7 @@ let gameBoard = {
     }
 
     
-        const player1 = playerMaker("Marco", "x");
+     const player1 = playerMaker("Marco", "x");
     
         const player2 = playerMaker("King", "y");
     
@@ -125,6 +175,7 @@ const gameController = {
 
     activeSign: "x",
     turnCounter: 1,
+    fullfields: 0,
     roundCounter: 0,
     
     
@@ -141,29 +192,44 @@ const gameController = {
     appendSign: (x, y, activeSign) => {
         gameBoard.field[x][y] = activeSign;
         gameBoard.checkForWin(activeSign, x, y);
-        
+        gameController.fullfields += 1;
         gameController.switchPlayerTurn(gameController.turnCounter);
     },
     updateScore: (activeSign) => {
 
         roundFields[gameController.roundCounter].textContent = "X";
         
-        console.log(player1.playerSign);
-        console.log(player1.playerSign == activeSign);
+        
         if(player1.playerSign == activeSign){
+            player1.wonRounds += 1;
             player1Scores[gameController.roundCounter].textContent = "+";
             player2Scores[gameController.roundCounter].textContent = "-";
         }
         else if(player2.playerSign == activeSign){
+            player2.wonRounds += 1;
             player1Scores[gameController.roundCounter].textContent = "-";
             player2Scores[gameController.roundCounter].textContent = "+";
         }
+        
         else {
+            player1.wonRounds += 0.5;
+            player2.wonRounds += 0.5;
+            player1Scores[gameController.roundCounter].textContent = "%";
+            player2Scores[gameController.roundCounter].textContent = "%";
 
         }
-        gameController.roundCounter += 1;
+        if(gameController.roundCounter != 18) {
+            gameController.roundCounter += 1;
+        }
+        
 
+        
+        
         setTimeout(() => {
+            if(gameController.roundCounter == 18){
+            DisplayFinalResults(player1, player2);
+            gameController.roundCounter = 0;
+        }
             restartBoard();
         }, 1000)
         
@@ -173,6 +239,46 @@ const gameController = {
 
 }
 
+function DisplayFinalResults(player1, player2){
+
+    if(player1.wonRounds > player2.wonRounds) {
+        alert(player1.name + " won with " + player1.wonRounds + " rounds to " + player2.wonRounds + " that " + player2.name + " had!");
+        player1ScoresArray.forEach((player1ScoreField) => {
+            player1ScoreField.textContent = "a";
+        })
+        player2ScoresArray.forEach((player2ScoreField) => {
+            player2ScoreField.textContent = "a";
+        })
+        roundsFieldsArray.forEach((roundsScoreField) => {
+            roundsScoreField.textContent = "a";
+        })
+    }
+    else if( player2.wonRounds > player1.wonRounds) {
+        alert(player2.name + " won with " + player2.wonRounds + " rounds to " + player1.wonRounds + " that " + player1.name + " had!");
+        player1ScoresArray.forEach((player1ScoreField) => {
+            player1ScoreField.textContent = "a";
+        })
+        player2ScoresArray.forEach((player2ScoreField) => {
+            player2ScoreField.textContent = "a";
+        })
+        roundsFieldsArray.forEach((roundsScoreField) => {
+            roundsScoreField.textContent = "a";
+        })
+    }
+
+    else {
+        alert("Its a tigh with both players having " + player1.wonRounds);
+        player1ScoresArray.forEach((player1ScoreField) => {
+            player1ScoreField.textContent = "a";
+        })
+        player2ScoresArray.forEach((player2ScoreField) => {
+            player2ScoreField.textContent = "a";
+        })
+        roundsFieldsArray.forEach((roundsScoreField) => {
+            roundsScoreField.textContent = "a";
+        })
+    }
+}
 
 
 function allowPlaying(){
@@ -203,6 +309,10 @@ function disablePlaying(){
 }
 
 function restartBoard(){
+
+    gameController.fullfields = 0;
+    gameController.turnCounter = 1;
+    gameController.activeSign = "x";
     fieldsArray.forEach((field) => {
         
         field.textContent = '';
@@ -217,21 +327,7 @@ function restartBoard(){
 
 }
 
-/* for( let i = 0; i < 9; i++){
 
-    fields[i].addEventListener('click', (e) => {
-        const x = e.target.getAttribute('data-x');
-        const y = e.target.getAttribute('data-y');
-
-
-        fields[i].textContent = gameController.activeSign;
-        gameController.appendSign(x, y, gameController.activeSign);
-        console.log(gameBoard.field);
-        e.target.removeEventListener('click', (e));
-
-
-    })
-} */
 allowPlaying();
 console.log(gameBoard.field);
 
